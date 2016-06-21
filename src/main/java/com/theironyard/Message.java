ab01bdf2da9476b4a83d2ba9b585f89664833b4f
willriggins <williamriggins2013@gmail.com>
2016-06-21 16:31:10 -0400

    sql support

--- src/main/java/com/theironyard/Message.java
+++ src/main/java/com/theironyard/Message.java
@@ -1,10 +1,18 @@
 package com.theironyard;
 
+import javax.persistence.*;
+
 /**
  * Created by will on 6/20/16.
  */
+@Entity
+@Table(name = "microblog")
 public class Message {
+    @Id
+    @GeneratedValue
     Integer id;
+
+    @Column(nullable = false)
     String text;
 
     public Message(Integer id, String text) {
---
+++ src/main/java/com/theironyard/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.theironyard;
+
+/**
+ * Created by will on 6/21/16.
+ */
+public interface MessageRepository {
+}
